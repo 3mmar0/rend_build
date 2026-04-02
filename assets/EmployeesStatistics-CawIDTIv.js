9246b0da58d621bdb72160ae00cde4279870ebac
= <=>
2026-04-02 17:29:37 +0200

    builld - 14

---
+++ assets/EmployeesStatistics-CawIDTIv.js
@@ -0,0 +1,2 @@
+import{j as s}from"./index-hX-55792.js";import{S as r}from"./StatisticsCards-BydGkt7q.js";function a({cards:t}){return s.jsx(r,{cards:t,numJustifyStart:!0})}export{a as E};
+//# sourceMappingURL=EmployeesStatistics-CawIDTIv.js.map

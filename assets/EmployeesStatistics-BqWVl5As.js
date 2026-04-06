0aa6518d88ecc7062edad507ce2bfeaf1b2d671f
= <=>
2026-04-06 11:30:10 +0200

    build - 19

---
+++ assets/EmployeesStatistics-BqWVl5As.js
@@ -0,0 +1,2 @@
+import{j as s}from"./index-DdF_sz2v.js";import{S as r}from"./StatisticsCards-D6ephEL-.js";function a({cards:t}){return s.jsx(r,{cards:t,numJustifyStart:!0})}export{a as E};
+//# sourceMappingURL=EmployeesStatistics-BqWVl5As.js.map

1027187ee3f712a51853d3ca2b22c6d64e7ce375
= <=>
2026-04-06 15:04:14 +0200

    build - 24

---
+++ assets/EmployeesStatistics-BIpgFF69.js
@@ -0,0 +1,2 @@
+import{j as s}from"./index-DaQF0f97.js";import{S as r}from"./StatisticsCards-D_F-zw4y.js";function a({cards:t}){return s.jsx(r,{cards:t,numJustifyStart:!0})}export{a as E};
+//# sourceMappingURL=EmployeesStatistics-BIpgFF69.js.map
---
+++ assets/SelfServices.page-O4AnNlC_.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-DaQF0f97.js";function i(){return e.jsx("div",{children:"SelfServices"})}export{i as default};
+//# sourceMappingURL=SelfServices.page-O4AnNlC_.js.map

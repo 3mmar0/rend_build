32c05d7661e8898d3f2bd95823e3cd08af703357
= <=>
2026-04-07 22:51:01 +0200

    Update index.html to reference new asset files, remove obsolete maintenance center schema and allowance components, and delete unused booking files for improved project organization and code cleanliness.

---
+++ assets/EmployeesStatistics-DtJ-rc55.js
@@ -0,0 +1,2 @@
+import{j as s}from"./index-BdWWPe3D.js";import{S as r}from"./StatisticsCards-C2BZWjCy.js";function a({cards:t}){return s.jsx(r,{cards:t,numJustifyStart:!0})}export{a as E};
+//# sourceMappingURL=EmployeesStatistics-DtJ-rc55.js.map
---
+++ assets/SelfServices.page-mHyptv16.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-BdWWPe3D.js";function i(){return e.jsx("div",{children:"SelfServices"})}export{i as default};
+//# sourceMappingURL=SelfServices.page-mHyptv16.js.map

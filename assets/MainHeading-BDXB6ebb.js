f9d374368c716c49976ace573148d37ea23fde84
= <=>
2026-04-22 00:37:24 +0200

    39

---
+++ assets/MainHeading-BDXB6ebb.js
@@ -0,0 +1,2 @@
+import{j as t,c as n}from"./index-BBXaoXGk.js";const i=({text:e,className:r})=>t.jsx("div",{className:n("w-full text-primery-blue-500 border-b border-primery-blue-500 pb-[5.5px] font-bold text-[22px] leading-[33px] tracking-normal text-center mb-4",r),children:e});export{i as M};
+//# sourceMappingURL=MainHeading-BDXB6ebb.js.map
---
+++ assets/SelfServices.page-CSGtrClL.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-BBXaoXGk.js";function i(){return e.jsx("div",{children:"SelfServices"})}export{i as default};
+//# sourceMappingURL=SelfServices.page-CSGtrClL.js.map
